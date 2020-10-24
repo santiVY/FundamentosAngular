126e3570ebf3b5bff41944a76cc0c7bee2e81d1a
Santiago Velez <svy9602@hotmail.com>
2020-10-24 17:45:22 -0500

    Se corrige la validacion de fechas

--- clone-airbnb/src/app/booking/components/form-booking/form-booking.component.ts
+++ clone-airbnb/src/app/booking/components/form-booking/form-booking.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { throwError } from 'rxjs';
 import { BookingService } from 'src/app/services/booking/booking.service';
 import { IBooking } from 'src/app/shared/models/booking.model';
 
@@ -12,7 +11,7 @@ import { IBooking } from 'src/app/shared/models/booking.model';
 export class FormBookingComponent implements OnInit {
 
   public formGroupBooking: FormGroup;
-
+  
   constructor(
     private formBuilder: FormBuilder,
     private bookingService: BookingService
@@ -25,7 +24,7 @@ export class FormBookingComponent implements OnInit {
   private formBookingInit():void {
     this.formGroupBooking = this.formBuilder.group({
       booking_date_start: ['', [Validators.required, this.validateDate]],
-      booking_date_end: ['', [Validators.required, this.validateDate]],
+      booking_date_end: ['', [Validators.required, this.validateDateEnd]],
       comments: ['', Validators.required]
     });
   }
@@ -33,15 +32,38 @@ export class FormBookingComponent implements OnInit {
   public validateDate(control: AbstractControl){
   
     const date = control.value;
+    localStorage.setItem('dateStart', date);
     const dateNow = new Date();
     let errors = null;
-    let caracter = '-';
-    let arrayDate = date.split(caracter);
+    let arrayDate = date.split('-');
 
-    if(parseInt(arrayDate[0]) <= dateNow.getFullYear() &&
-        parseInt(arrayDate[1]) <= (dateNow.getMonth() + 1) &&
-        parseInt(arrayDate[2]) < dateNow.getDate()) {
-        errors = { dateError: 'La fecha debe ser mayor o igual a la fecha actual'};
+    if(parseInt(arrayDate[0]) < dateNow.getFullYear()){
+      errors = { dateStartError: 'El año debe ser mayor o igual al año actual'};
+    }else if(parseInt(arrayDate[0]) === dateNow.getFullYear() && parseInt(arrayDate[1]) < (dateNow.getMonth() + 1)) {
+      errors = { dateStartError: 'El mes debe ser mayor o igual al mes actual'};
+    }else if(parseInt(arrayDate[0]) === dateNow.getFullYear() && parseInt(arrayDate[1]) === (dateNow.getMonth() + 1) && parseInt(arrayDate[2]) < dateNow.getDate()){
+      errors = { dateStartError: 'El dia debe ser mayor o igual al dia actual'};
+    }
+
+    return errors;
+  }
+
+  public validateDateEnd(control: AbstractControl){
+    let dateEnd = control.value;
+    let dateStar = localStorage.getItem('dateStart');
+    let errors = null;
+    let arrayDateStart = dateStar.split('-');
+    let arrayDateEnd = dateEnd.split('-');
+
+    console.log('fecha inicio', arrayDateStart)
+    console.log('fecha final', arrayDateEnd)
+
+    if(parseInt(arrayDateEnd[0]) < parseInt(arrayDateStart[0])){
+      errors = { dateStartError: 'El año debe ser mayor o igual al año inicio'};
+    }else if(parseInt(arrayDateEnd[0]) === parseInt(arrayDateStart[0]) && parseInt(arrayDateEnd[1]) < parseInt(arrayDateStart[1])) {
+      errors = { dateStartError: 'El mes debe ser mayor o igual al mes inicio'};
+    }else if(parseInt(arrayDateEnd[0]) === parseInt(arrayDateStart[0]) && parseInt(arrayDateEnd[1]) === parseInt(arrayDateStart[1]) && parseInt(arrayDateEnd[2]) < parseInt(arrayDateStart[2])){
+      errors = { dateStartError: 'El dia debe ser mayor o igual al dia inicio'};
     }
 
     return errors;
@@ -64,9 +86,14 @@ export class FormBookingComponent implements OnInit {
       errorMessage += 'Campo requerido. ';
     }
 
-    if(errors.dateError){
-      errorMessage += errors.dateError;
+    if(errors.dateStartError){
+      errorMessage += errors.dateStartError;
     }
+
+    if(errors.dateEndError){
+      errorMessage += errors.dateEndError;
+    }
+    
     return errorMessage;
   }
 
